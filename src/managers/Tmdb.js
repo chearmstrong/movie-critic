7c6d7c74336d9f7c18a19d9a529e764f6f209e93
Che Armstrong <neuroscience.che@gmail.com>
2020-02-18 10:51:29 +0000

    Comment

--- src/managers/Tmdb.js
+++ src/managers/Tmdb.js
@@ -6,7 +6,7 @@ const { apiKey, baseUrl, imageBaseUrl } = API.TMDB
 
 const getMoviePoster = async id => {
   const url = `${baseUrl}/movie/${id}/images?api_key=${apiKey}&language=en`
-  const response = await needle('get', url).catch(() => null)
+  const response = await needle('get', url).catch(() => null) // JSON === true not needed
   const filePath = R.path(['body', 'posters', 0, 'file_path'], response)
 
   return filePath ? `${imageBaseUrl}/w500${filePath}` : null
